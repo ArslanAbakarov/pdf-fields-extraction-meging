b0db941f21eb60c9772fa67cb18541c7b006eb44
ArslanAbakarov <arslan.abakarov@ami.ca>
2025-05-13 17:18:36 -0400

    Fixing Env script

--- poc-3/renamer-v2.8/setup_env.sh
+++ poc-3/renamer-v2.8/setup_env.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-
 ENV_NAME="xenex"
 
 echo "[*] Deleting old env if exists"
@@ -19,33 +18,23 @@ pip install \
     fastapi \
     uvicorn
 
-
 pip install python-multipart
 pip install pymupdf
-
 pip install paddleocr
 python -c "from paddleocr import PaddleOCR; ocr = PaddleOCR()"
-
 pip install numpy==1.24.4 scipy==1.10.1    
-
-conda install -c conda-forge faiss-cpu
-onda install -c conda-forge numpy=1.24.4
-conda install -c conda-forge pytorch
-conda install -c tqdm
-
+pip install torch torchvision torchaudio sentence-transformers
 pip install torch==2.4.0 torchvision==0.19.0 torchaudio==2.4.0
-
 pip install paddleocr==2.6.1.1
-# pip install paddlepaddle==2.4.2 -f https://www.paddlepaddle.org.cn/whl/linux/mavl.html
 pip install paddlepaddle==2.5.2 -f https://www.paddlepaddle.org.cn/whl/linux/mavl.html
 
-pip install torch torchvision torchaudio sentence-transformers
-
+conda install -c conda-forge faiss-cpu
+conda install -c conda-forge numpy=1.24.4
+conda install -c conda-forge pytorch
+conda install -c tqdm
 conda install -c conda-forge opencv
 
 echo "[*] Activating env"
 source "$(conda info --base)/etc/profile.d/conda.sh"
-conda activate "$ENV_NAME"
-
 echo "[✓] Environment '$ENV_NAME' is ready."
 conda activate xenex
