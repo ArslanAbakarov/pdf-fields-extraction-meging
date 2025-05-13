5fe21dc33ed280b31dcb4aa1852e8c25bfc67c8b
ArslanAbakarov <arslan.abakarov@ami.ca>
2025-05-13 15:04:43 -0400

    Updating Setup ENV  file

--- poc-3/renamer-v2.8/setup_env.sh
+++ poc-3/renamer-v2.8/setup_env.sh
@@ -6,7 +6,7 @@ echo "[*] Deleting old env if exists"
 conda remove -n "$ENV_NAME" --all -y
 
 echo "[*] Creating new env: $ENV_NAME"
-conda create -n "$ENV_NAME" python=3.9 numpy=1.26 -y
+conda create -n "$ENV_NAME" python=3.12 numpy=1.26 -y
 
 echo "[*] Installing pip packages"
 pip install \
@@ -17,12 +17,22 @@ pip install \
     rapidfuzz \
     tqdm
 
+pip install pymupdf
+
+pip install numpy==1.24.4 scipy==1.10.1    
+
+conda install -c conda-forge faiss-cpu
+onda install -c conda-forge numpy=1.24.4
+conda install -c conda-forge pytorch
+conda install -c tqdm  
+
+pip install torch torchvision torchaudio sentence-transformers
+
+conda install -c conda-forge opencv
+
 echo "[*] Activating env"
 source "$(conda info --base)/etc/profile.d/conda.sh"
 conda activate "$ENV_NAME"
-
-echo "[*] Installing Conda packages"
-conda install -y -c conda-forge faiss-cpu
 
 echo "[✓] Environment '$ENV_NAME' is ready."
 conda activate xenex
