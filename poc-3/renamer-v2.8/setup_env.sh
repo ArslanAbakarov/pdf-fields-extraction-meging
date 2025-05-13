af552b0562a06683ffb7301ba79e5cda72528c7e
ArslanAbakarov <arslan.abakarov@ami.ca>
2025-05-13 17:37:31 -0400

    Updating setup_env.sh

--- poc-3/renamer-v2.8/setup_env.sh
+++ poc-3/renamer-v2.8/setup_env.sh
@@ -5,7 +5,7 @@ echo "[*] Deleting old env if exists"
 conda remove -n "$ENV_NAME" --all -y
 
 echo "[*] Creating new env: $ENV_NAME"
-conda create -n "$ENV_NAME" python=3.12 numpy=1.26 -y
+conda create -n "$ENV_NAME" python=3.11 numpy=1.26 -y
 conda activate "$ENV_NAME"
 
 echo "[*] Installing pip packages"
@@ -25,9 +25,13 @@ pip install paddleocr
 python -c "from paddleocr import PaddleOCR; ocr = PaddleOCR()"
 pip install numpy==1.24.4 scipy==1.10.1    
 pip install sentence-transformers
-pip install torch==2.4.0 torchvision==0.19.0 torchaudio==2.4.0
+# pip install torch==2.4.0 torchvision==0.19.0 torchaudio==2.4.0
 pip install paddleocr==2.6.1.1
 pip install paddlepaddle==2.5.2 -f https://www.paddlepaddle.org.cn/whl/linux/mavl.html
+
+conda install -y -c conda-forge \
+        pytorch==2.1.0 torchvision==0.16.0 \
+        faiss-cpu sentence-transformers transformers
 
 conda install -c conda-forge faiss-cpu -y
 conda install -c conda-forge numpy=1.24.4 -y
